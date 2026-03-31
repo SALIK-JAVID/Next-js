101f6d84b6f6e91f97e8de1d5009f6c21d1eede4
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-03-31 15:50:16 +0530

    first next app

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <h1>
+                this is the page componet showing automatic file based routing in next.js
+            </h1>
+        </div>
+    )
+}
